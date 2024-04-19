ee9aaa8bd9ff062ae52ae3651d0aec5797cec0a5
ismoildev007 <ismoil3436@gmail.com>
2024-04-19 14:47:27 +0500

    update qilindi

--- config/app.php
+++ config/app.php
@@ -7,7 +7,7 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Application Name
+    | Application Name   TEST
     |--------------------------------------------------------------------------
     |
     | This value is the name of your application. This value is used when the
